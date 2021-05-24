29ea5cdd100faebb7bf9acf8e11f46eb22fc1d9a
Mayuri <84445378+Mayuri1211@users.noreply.github.com>
2021-05-24 13:49:12 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,21 @@
+#Arithmetic Operators
+
+
+x = 15
+y = 4
+
+print('x + y =',x+y)
+
+
+print('x - y =',x-y)
+
+
+print('x * y =',x*y)
+
+
+print('x / y =',x/y)
+
+
+print('x // y =',x//y)
+
+print('x ** y =',x**y)
